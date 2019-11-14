0284050fd330c45d89ac89528848a58e510883e7
nmoskaleva <nsmoskaleva@gmail.com>
2019-11-14 14:28:06 -0800

    Updates

---
+++ precache-manifest.dc4e7bde1a14ff887fa2057098a6650f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f97e52da764193d11a6430286a66765",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "0f5f2a9d990bea83b83c",
+    "url": "/todo-app/static/css/main.a9ee2f53.chunk.css"
+  },
+  {
+    "revision": "e5640d6798fad0dcaf38",
+    "url": "/todo-app/static/js/2.cdfbc035.chunk.js"
+  },
+  {
+    "revision": "0f5f2a9d990bea83b83c",
+    "url": "/todo-app/static/js/main.8a816e93.chunk.js"
+  },
+  {
+    "revision": "52fc42179e35f7eb9c42",
+    "url": "/todo-app/static/js/runtime-main.117f3ccd.js"
+  }
+]);
